359f6644356d9f655d2889a60daa160019701b52
dabartmess <dabartmess@proton.me>
2025-05-15 05:29:39 -0600

    Completed pulling of song metadata.. Now on to putting that info back into the files that are screwed up

--- mb_search.py
+++ mb_search.py
@@ -11,15 +11,14 @@ from musicbrainzngs import search_releases, search_artists, browse_releases, bro
     musicbrainz, ResponseError
 from requests import HTTPError
 
-# track
-trackrec = [{"id": "", "title": ""}]
-# release
-subrec = [{"id":"", "title":"", "country": "", "date": "", "tracks":[]}]
-# main record
-rec = {"id": "", "name": "", "genre": "", "albums": []}
-
 id = ""
 name_record = {}
+# track
+trackrec = [{"id":"", "title":""}]
+# release
+subrec = [{"id":"", "title":"", "country":"", "date":"", "label":"", "tracks":[]}]
+# main record
+rec = {"id":"", "name":"", "genre":"", "albums":[]}
 
 set_useragent(
     "py_mb_update",
@@ -46,18 +45,25 @@ def get_genre(resultfac: {}):
     return tmp_dict
 
 def get_metadata(artist, results: {}):
+    global rec
     global subrec
+    global tracks
+
+    rec = {"id":"", "name":"", "genre":"", "albums":[]}
+
     print("Entering get_metadata")
     results = search_artists(query=artist)['artist-list'][0]
     rec["id"] = results['id']
     rec["name"] = results['name']
     rec["genre"] = get_genre(results)
 
-    print("Artist Info:")
+    # main record
     print(rec["id"], "/", rec["name"], "/", rec["genre"])
 
     releases = []
+    print("Artist Info:")
     try:
+        #releases = search_releases(query='artist=rec["name"]')
         releases = search_releases(query="discids", artist=rec["name"])
     except musicbrainz.InvalidFilterError as exc:
         print("Invalid Filter Error: ", exc.msg)
@@ -66,88 +72,67 @@ def get_metadata(artist, results: {}):
     except musicbrainz.ResponseError as exc:
         if "Bad Request" not in str(exc.cause):
             print("Response Error: ", exc.cause)
-    print(releases)
+    print("Releases: ",releases)
 
-    subrec2 = {}
-    for album in releases["release-list"]:
-        subrec2 = {album["id"], album["title"]}
-        if "release-event-list" in album:
-            subrec2 = {"id":album["id"], "title":album["title"], "date":album["release-event-list"][0]["date"],
-                       "country": album["release-event-list"][0]["area"]["name"], "tracks":[]}
-        else:
-            subrec2 = {"id":album["id"], "title":album["title"], "date": "", "country": "", "tracks":[]}
+    for f in releases["release-list"]:
+        # release
+        subrec = [{"id":"", "title":"", "country":"", "date":"", "label":"", "tracks":[]}]
+        subrec2 = {}
+        for album in releases["release-list"]:
 
-        recordings = {}
-        try:
-    #        print(releases["release-list"][0]["title"])
-            recordings = browse_recordings(release=releases["release-list"][0]["id"])
-        except musicbrainz.InvalidFilterError as exc:
-            print("Invalid Filter Error: ", exc.msg)
-        except urllib.error.HTTPError as exc:
-            print("HTTP Error: ", exc.reason)
-        except musicbrainz.ResponseError as exc:
-            if "Bad Request" not in str(exc.cause):
-                print("Response Error: ", exc.cause)
-        print("Got Recordings", recordings)
-        print("Disc Info:")
-        print("Tracks:")
+            print()
+            subrec2 = {album["id"], album["title"]}
+            if "release-event-list" in album and "label-info-list" in album:
+                subrec2 = {"id":album["id"], "title":album["title"], "country":album["release-event-list"][0]["area"]["name"],
+                        "date":album["release-event-list"][0]["date"],
+                        "label":album["label-info-list"][0]["label"]["name"], "tracks":[]}
+                print("Disc: ", album["title"], "/", album["release-event-list"][0]["date"], "/",
+                      album["release-event-list"][0]["area"]["name"], "/", album["id"])
+            else:
+                subrec2 = {"id":album["id"], "title":album["title"], "date": "", "country": "", "tracks":[]}
+                print("Disc: ", album["title"], "/", album["id"])
 
-        tracks = []
-        for recording in recordings["recording-list"]:
-            print("Recording: ",recording)
-            trackrec.append({"id": recording["id"], "title": recording["title"]})
+            recordings = {}
+            try:
+        #        print(releases["release-list"][0]["title"])
+                recordings = browse_recordings(release=releases["release-list"][0]["id"])
+            except musicbrainz.InvalidFilterError as exc:
+                print("Invalid Filter Error: ", exc.msg)
+            except urllib.error.HTTPError as exc:
+                print("HTTP Error: ", exc.reason)
+            except musicbrainz.ResponseError as exc:
+                if "Bad Request" not in str(exc.cause):
+                    print("Response Error: ", exc.cause)
+            print("Tracks:")
 
-        subrec2["tracks"].append(trackrec)
+            tracks = []
+            # track
+            trackrec = [{"id":"", "title":""}]
 
-    subrec = subrec2
+            for recording in recordings["recording-list"]:
+                #print("Track: ",recording["title"], "/", recording["id"])
+                trackrec.append({"id": recording["id"], "title": recording["title"]})
 
-    rec["albums"].append(subrec)
+            subrec2["tracks"].append(trackrec)
+
+        subrec = subrec2
+
+        rec["albums"].append(subrec)
 
     return rec
-
-def search_by_artist(artist, entity_type='artist'):
-    print("Entering search_by_artist")
-    set_useragent(
-        "py_mb_update",
-        "Dev-0.9",
-        "https://github.com/dabartmess/py_mb_update/"
-    )
-    artist_list = search_artists(query=artist)['artist-list'][0]
-#    structure = artist_list[0]
-#    genre = get_genre(artist_list)
-    return artist_list
-
-
-def search_by_genre(genre, artist, entity_type='artist'):
-    print("Entering search_by_genre")
-    genre = None
-    if entity_type == 'artist':
-        result = search_artists(query={artist})['artist-list'][0]
-        print("release-list: ", result['release-list'])
-        #genre = get_genre(result)
-    elif entity_type == 'release':
-        result = search_releases(query="{release}")
-        #print("result: ", result)
-        print("release-list: ", result)
-        #genre = get_genre(rec["name"])
-    else:
-        raise ValueError("Invalid entity type. Choose 'artist' or 'release'.")
-    return result
 
 if __name__ == '__main__':
     ### get first release
     # if len(sys.argv) > 1:
     # artist, album = [sys.argv[1], sys.argv[2]]
 
-    results = search_by_artist("Louis Armstrong", 'artist')
-    set_useragent(
-        "py_mb_update",
-        "Dev-0.9",
-        "https://github.com/dabartmess/py_mb_update/"
-    )
+    artist = "Louis Armstrong"
+    results = search_artists(query=artist)['artist-list'][0]
     record_tmp = get_metadata(results["name"], results)
 
-    json_obj = json.dumps(record_tmp, indent=3)
+    json_obj = json.dumps(record_tmp)
 
     #results, genre = search_by_genre(genre, "Louis Armstrong", "release")
     print(json_obj)
+    with open("ography.json", "w+") as json_file:
+        json.dump(json_obj, json_file)
